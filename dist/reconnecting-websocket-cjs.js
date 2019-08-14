1e547073a55228e5489dfd88bd5b1354d9c276ed
synfonaut <synfonaut@protonmail.com>
2019-08-13 23:13:31 -0700

    Revert "build"
    This reverts commit 6cac4bc1f2dffe34e07e8dbf941340f7647b8713.

--- dist/reconnecting-websocket-cjs.js
+++ dist/reconnecting-websocket-cjs.js
@@ -393,7 +393,7 @@ var ReconnectingWebSocket = /** @class */ (function () {
     };
     ReconnectingWebSocket.prototype._getNextDelay = function () {
         var _a = this._options, _b = _a.reconnectionDelayGrowFactor, reconnectionDelayGrowFactor = _b === void 0 ? DEFAULT.reconnectionDelayGrowFactor : _b, _c = _a.minReconnectionDelay, minReconnectionDelay = _c === void 0 ? DEFAULT.minReconnectionDelay : _c, _d = _a.maxReconnectionDelay, maxReconnectionDelay = _d === void 0 ? DEFAULT.maxReconnectionDelay : _d;
-        var delay = 0;
+        var delay = minReconnectionDelay;
         if (this._retryCount > 0) {
             delay =
                 minReconnectionDelay * Math.pow(reconnectionDelayGrowFactor, this._retryCount - 1);
